6f8ef0d229d6a67ceffe679f54902ee7bb527e84
FinlayS <fsgm01@gmail.com>
2018-09-23 09:51:14 +0100

    S3 L14 - adding Yargs

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 console.log('Starting app.js');
 
-// from nodejs.org/api
 const fs = require('fs');
-const _ =require('lodash');
+const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command:',  command);
+console.log('YARGS:', argv);
 
 
-
+if (command === 'add') {
+    notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+    notes.getAll();
+} else if (command === 'read') {
+    notes.getNote(argv.title);
+} else if (command === 'remove') {
+    notes.removeNote(argv.title);
+}
+else {
+    console.log('Command not recognised');
+}
--- notes.js
+++ notes.js
@@ -1,8 +1,30 @@
 console.log('Starting notes.js');
 
-// console.log(module)
 
-module.exports.addNote =  () => {
-   console.log('addNote');
-   return 'New Note';
+var addNote = (title, body) => {
+   console.log('Adding note', title, body);
+
 };
+
+var getAll = () => {
+ console.log('Getting all notes')
+
+};
+
+var getNote = (title) => {
+    console.log('Getting note', title);
+
+};
+
+var removeNote = (title) => {
+    console.log('Removing note', title);
+
+};
+
+module.exports = {
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
+
