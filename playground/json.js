6a4204d968dd2cee2d3016691da03a921b708691
FinlayS <fsgm01@gmail.com>
2018-09-23 10:23:15 +0100

    S3 L15 - introducing JSON

---
+++ playground/json.js
@@ -0,0 +1,33 @@
+// var obj = {
+//     name: 'Finlay'
+// };
+//
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj)
+// console.log(stringObj);
+
+// var personString =  '{"name": "Finlay", "age": 53}';
+// var person = JSON.parse(personString)
+//
+// console.log(typeof person)
+// console.log(person)
+
+const fs = require('fs');
+
+var originalNote = {
+    title: 'Some title',
+    body: 'Some - body'
+}
+
+// originalNoteString
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+
+var noteString =  fs.readFileSync('notes.json');
+// note
+var note = JSON.parse(noteString);
+
+
+console.log(typeof note);
+console.log(note.title);
